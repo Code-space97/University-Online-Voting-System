b4c08b77bee602fcc088e5d2202e2d2a5e5c4e79
Lokesh Joshi <dheerajjoshi326@gmail.com>
2026-04-13 09:50:19 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <div class="container">
+    <div class="pull-right hidden-xs">
+      <b>Built by Lokesh</b>
+    </div>
+    <strong>Copyright &copy; 2026 <a href="https://prsuniv.ac.in/">PRSU</a></strong>
+  </div>
+  <!-- /.container -->
+</footer>
